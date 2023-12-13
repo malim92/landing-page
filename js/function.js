8eb4ea823a3dfafaf6e75203069c2a2889a4cee3
Ali <malim92@live.com>
2023-12-13 23:10:57 +0200

    added rest of the sections, project done

--- js/function.js
+++ js/function.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var map = L.map('map').setView([51.505, -0.09], 13); // Set the initial coordinates and zoom level
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map); // Add OpenStreetMap tiles
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   });
+}
